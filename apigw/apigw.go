6a5c7cbadaee377087efdf8ea2130f536b108360
mloves0824 <mloves0824@163.com>
2018-10-08 17:45:57 +0800

    test

--- apigw/apigw.go
+++ apigw/apigw.go
@@ -1,7 +1,104 @@
-import "fmt"
-
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	pb "github.com/mloves0824/antalk-go/proto"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	name    string
+	subStrm map[string]pb.ApigwService_SubscribeServer
+}
+
+func newServer() *server {
+	return &server{name: "ApigwServer",
+		subStrm: make(map[string]pb.ApigwService_SubscribeServer)}
+}
+
+func (s *server) Login(ctx context.Context, req *pb.LoginReq) (*pb.LoginResp, error) {
+	log.Printf("Received a Login Request (%s)", req.GetUid())
+	value, ok := s.subStrm[req.GetUid()+":KICKOUT"]
+	if ok {
+		log.Printf("Uid %s already Login!", req.GetUid())
+		if err := value.Send(&pb.Notification{Type: pb.TopicType_KICKOUT, Kick: &pb.KickNotify{Uid: req.GetUid(), Reason: 0}}); err != nil {
+			log.Fatalf("Send failed %v", err)
+		}
+	}
+	return &pb.LoginResp{Uid: req.GetUid(), ResultCode: 0}, nil
+}
+
+func (s *server) Logout(ctx context.Context, req *pb.LogoutReq) (*pb.LogoutResp, error) {
+	log.Printf("Received a Logout Request (%s)", req.GetUid())
+	return &pb.LogoutResp{}, nil
+}
+
+func (s *server) Subscribe(stream pb.ApigwService_SubscribeServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Received a Subscription Request (%s, %s)", in.GetUid(), in.GetType().String())
+		_, ok := s.subStrm[in.GetUid()+":"+in.GetType().String()]
+		if !ok {
+			s.subStrm[in.GetUid()+":"+in.GetType().String()] = stream
+		} else {
+			log.Printf("Client %s already subscribed to %s", in.GetUid(), in.GetType().String())
+		}
+	}
+	return nil
+}
+
+func (s *server) pushUpdates() {
+	var str string
+	//var i uint32
+
+	time.Sleep(1 * time.Second)
+
+	for {
+		//fmt.Println("walk thru the subscribe streams")
+		for k, v := range s.subStrm {
+			if strings.Contains(k, "KICKOUT") {
+				fmt.Printf("Enter new KICKOUT for Client(%v): ", k)
+				fmt.Scan(&str)
+				if err := v.Send(&pb.Notification{Type: pb.TopicType_KICKOUT, Kick: &pb.KickNotify{Uid: k, Reason: 0}}); err != nil {
+					log.Fatalf("Send failed %v", err)
+				}
+			} else {
+				fmt.Printf("Not Support!", k)
+			}
+		}
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	myServer := newServer()
+	pb.RegisterApigwServiceServer(s, myServer)
+
+	//go myServer.pushUpdates()
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
---
+++ client/client.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	//	"math/rand"
+	//	"time"
+
+	"github.com/docopt/docopt-go"
+	pb "github.com/mloves0824/antalk-go/proto"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var name string
+var subChan chan pb.TopicType
+
+const (
+	address = "localhost:50051"
+)
+
+// Login ...
+func login(client pb.ApigwServiceClient) error {
+	//log.Printf("Calling Register RPC")
+
+	resp, err := client.Login(context.Background(), &pb.LoginReq{Uid: name})
+	if err != nil {
+		log.Fatalf("Login failed %v", err)
+	}
+	log.Println("Login Resp: ", resp, err)
+
+	return nil
+}
+
+// subscribeToModeChanges ...
+func subscribeTopicKickout(c chan pb.TopicType) {
+	log.Println("Subscribing to Kickout")
+	c <- pb.TopicType_KICKOUT
+}
+
+// recvNotification ...
+func recvNotification(stream pb.ApigwService_SubscribeClient) {
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("failed to recv %v", err)
+		}
+		if err == io.EOF {
+			break
+		}
+		log.Print(resp, err)
+	}
+}
+
+// subscribe ...
+func subscribe(client pb.ApigwServiceClient, c chan pb.TopicType) {
+	stream, err := client.Subscribe(context.Background())
+	if err != nil {
+		log.Fatalf("failed to subscribe %v", err)
+	}
+
+	go recvNotification(stream)
+
+	for t := range c {
+		//log.Printf("sending subscibe for topic %s", t.String())
+		if err := stream.Send(&pb.Topic{Uid: name, Type: t}); err != nil {
+			log.Fatalf("couldnt send %s", t.String())
+		}
+	}
+	stream.CloseSend()
+}
+
+func Argument(d map[string]interface{}, name string) (string, bool) {
+	if d[name] != nil {
+		if s, ok := d[name].(string); ok {
+			if s != "" {
+				return s, true
+			}
+			log.Panicf("option %s requires an argument", name)
+		} else {
+			log.Panicf("option %s isn't a valid string", name)
+		}
+	}
+	return "", false
+}
+
+func main() {
+
+	const usage = `
+Usage:
+	client --uid=uid
+`
+	d, err := docopt.Parse(usage, nil, true, "", false)
+	if err != nil {
+		log.Panicf("parse arguments failed: %s", err)
+	}
+
+	if s, ok := Argument(d, "--uid"); ok {
+		fmt.Printf("uid=%s\n", s)
+		name = s
+	}
+	// init important structures
+	subChan = make(chan pb.TopicType, 10)
+	//	rand.Seed(time.Now().UTC().UnixNano())
+	//	name = fmt.Sprintf("%s:%d", "Client", rand.Intn(50))
+	//name = s
+
+	// Setup a connection with the server
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	client := pb.NewApigwServiceClient(conn)
+
+	login(client)
+
+	subscribeTopicKickout(subChan)
+	subscribe(client, subChan)
+}
