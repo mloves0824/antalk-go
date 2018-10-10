cdd46b061ba307b9dad48749d066bb8f41029766
mloves0824 <mloves0824@163.com>
2018-10-10 20:04:47 +0800

    tst

--- auth/auth.go
+++ auth/auth.go
@@ -9,21 +9,40 @@ import (
 	//"time"
 
 	pb "github.com/mloves0824/antalk-go/proto/auth"
+	common_pb "github.com/mloves0824/antalk-go/proto/common"
+	data_pb "github.com/mloves0824/antalk-go/proto/data"
+
 	context "golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
-	name string
+	name        string
+	data_client data_pb.DataServiceClient
 }
+
+const (
+	data_addr string = "localhost:50052"
+)
 
 func newServer() *server {
-	return &server{name: "AuthServer"}
+	conn, err := grpc.Dial(data_addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to Dial DataSvr: %v", err)
+	}
+	new_data_client := data_pb.NewDataServiceClient(conn)
+	return &server{name: "AuthServer", data_client: new_data_client}
 }
 
-func (s *server) CheckAuth(context.Context, *pb.CheckAuthReq) (*pb.CheckAuthResp, error) {
-	return &pb.CheckAuthResp{}, nil
+func (s *server) CheckAuth(ctx context.Context, req *pb.CheckAuthReq) (*pb.CheckAuthResp, error) {
+	data_req := &data_pb.CheckAuthReq{Uid: req.GetUid(), Token: req.GetToken()}
+	data_resp, err := s.data_client.CheckAuth(context.Background(), data_req)
+	if err != nil {
+		return &pb.CheckAuthResp{Result: common_pb.ResultType_ResultErrCheckAuth}, nil
+	}
+	log.Printf("send CheckAuthReq success: result=%s", data_resp.GetResult())
+	return &pb.CheckAuthResp{Result: data_resp.GetResult()}, nil
 }
 
 func main() {
--- data/data.go
+++ data/data.go
@@ -5,11 +5,13 @@ import (
 	//"io"
 	"log"
 	"net"
-	//"strings"
+	//	"strings"
 	"time"
 
 	redis "github.com/mloves0824/antalk-go/pkg/utils/redis"
+	commonpb "github.com/mloves0824/antalk-go/proto/common"
 	pb "github.com/mloves0824/antalk-go/proto/data"
+
 	context "golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -26,13 +28,15 @@ type server struct {
 	redis_client *redis.Client
 }
 
-const ()
+const (
+	key_prefix_token = "TOEKN:"
+)
 
-var redis_conf redisConf = redisConf{"localhost:6379", "antalk-auth", 1}
+var redis_conf redisConf = redisConf{"127.0.0.1:19000", "antalk-auth", 10}
 
 func newServer() *server {
 
-	client, err := redis.NewClient(redis_conf.addr, redis_conf.auth, redis_conf.timeout)
+	client, err := redis.NewClientNoAuth(redis_conf.addr, redis_conf.timeout)
 	if err != nil {
 		log.Fatalf("failed to NewClient: %v", err)
 	}
@@ -40,12 +44,22 @@ func newServer() *server {
 	return &server{name: "DataServer", redis_client: client}
 }
 
-func (s *server) CheckAuth(context.Context, *pb.CheckAuthReq) (*pb.CheckAuthResp, error) {
-	return &pb.CheckAuthResp{}, nil
+func (s *server) CheckAuth(ctx context.Context, req *pb.CheckAuthReq) (*pb.CheckAuthResp, error) {
+	cmd := fmt.Sprintf("GET %s", key_prefix_token+req.GetToken())
+	value, err := s.redis_client.Do(cmd)
+	if err != nil {
+		return &pb.CheckAuthResp{Uid: req.GetUid(), Result: commonpb.ResultType_ResultErrRedis}, nil
+	}
+	if value != nil {
+		return &pb.CheckAuthResp{Uid: req.GetUid(), Result: commonpb.ResultType_ResultErrCheckAuth}, nil
+	}
+	return &pb.CheckAuthResp{Uid: req.GetUid(), Result: commonpb.ResultType_ResultOK}, nil
 }
+
 func (s *server) SetSession(context.Context, *pb.SetSessionReq) (*pb.SetSessionResp, error) {
 	return &pb.SetSessionResp{}, nil
 }
+
 func (s *server) GetSession(context.Context, *pb.GetSessionReq) (*pb.GetSessionResp, error) {
 	return &pb.GetSessionResp{}, nil
 }
