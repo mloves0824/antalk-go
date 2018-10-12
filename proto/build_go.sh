7b474972ff7f9e8ba187a30568a727101f424f08
mloves0824 <mloves0824@163.com>
2018-10-12 19:47:34 +0800

    test

--- proto/build_go.sh
+++ proto/build_go.sh
@@ -5,5 +5,6 @@ protoc ./apigw/*.proto --go_out=plugins=grpc:./
 protoc ./data/*.proto --go_out=plugins=grpc:./
 protoc ./auth/*.proto --go_out=plugins=grpc:./
 protoc ./msg/*.proto --go_out=plugins=grpc:./
+protoc ./seq/*.proto --go_out=plugins=grpc:./
 
-sed -i 's/common \"common\"/common \"github.com\/mloves0824\/antalk-go\/proto\/common\"/' ./apigw/apigw.pb.go ./data/data.pb.go ./auth/auth.pb.go ./msg/msg.pb.go
+sed -i 's/common \"common\"/common \"github.com\/mloves0824\/antalk-go\/proto\/common\"/' ./apigw/apigw.pb.go ./data/data.pb.go ./auth/auth.pb.go ./msg/msg.pb.go ./seq/seq.pb.go
